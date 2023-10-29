f45724b4cae79b5ff4ccaeb98b3a06314ed4c86a
mj00714 <144157036+mj00714@users.noreply.github.com>
2023-10-28 22:59:29 -0700

    activate

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "5180c64f810bffa47984d6ccf1abf30e"
+# weather_api_key = "5180c64f810bffa47984d6ccf1abf30e"
 
 # Geoapify API Key
-geoapify_key = "46198a1638ca44b591190d925120ca79"
+# geoapify_key = "46198a1638ca44b591190d925120ca79"
